9df040dd1dc48bb76a0efb01b095bb8424da6aa5
Hamza Hassan <hassanhamza930@gmail.com>
2022-09-29 11:16:31 +0500

    Minor Updates to UI and Animations

--- src/pages/landingPage/ui/landingPage.tsx
+++ src/pages/landingPage/ui/landingPage.tsx
@@ -5,23 +5,48 @@ import JobApplicationsThatFeelLikeAConvo from '../components/jobApplications';
 import SayGoodByeToExcelSheets from '../components/sayGoodByeToExcelSheet';
 import ScheduleInterviews from '../components/scheduleInterviews';
 import InterviewInApp from '../components/interviewInApp';
-
+import { motion } from "framer-motion";
 
 export default function LandingPage() {
     return (
         <div className="relative pt-[80px] h-full w-full flex flex-col justify-start items-center">
 
-            <div className="mt-16 text-7xl text-purple font-bold text-center">Recruit for<br />The Startup</div>
-            <div className="text-xl mt-10 text-purple font-regular text-center"><b>Wave</b> takes the pain out of recruiting world class talent.</div>
+            <div className="h-36 mt-16 text-7xl text-purple font-bold text-center overflow-hidden">
+                <motion.div
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true }}
+                    transition={{ duration: 0.7 }}
+                    variants={{
+                        visible: { opacity: 1, y: 0 },
+                        hidden: { opacity: 0, y: 100 }
+                    }}>
+                    Recruit for<br />The Startup
+                </motion.div>
+            </div>
+
+            <motion.div
+                initial="hidden"
+                whileInView="visible"
+                viewport={{ once: true }}
+                transition={{ duration: 0.7,delay:0.3 }}
+                variants={{
+                    visible: { opacity: 1, y: 0 },
+                    hidden: { opacity: 0, y: 50 },
+                }}
+                className="text-xl mt-10 text-purple font-regular text-center">
+                <b>Wave</b> takes the pain out of recruiting world class talent.
+            </motion.div>
+
 
             <ShufflingImages />
             <WeBelieveEveryApplicationDeservesToBeSeen />
-            <JobApplicationsThatFeelLikeAConvo/>
-            <SayGoodByeToExcelSheets/>
-            <ScheduleInterviews/>
-            <InterviewInApp/>
+            <JobApplicationsThatFeelLikeAConvo />
+            <SayGoodByeToExcelSheets />
+            <ScheduleInterviews />
+            <InterviewInApp />
 
 
-        </div>
+        </div >
     )
 }
