1172cef118197fef578039051ad796ddb292f122
Hamza Hassan <hassanhamza930@gmail.com>
2022-10-14 01:45:16 +0500

    Very Basic Changes to The Custom Questions page

--- src/pages/newJob/atoms/newJobAtoms.tsx
+++ src/pages/newJob/atoms/newJobAtoms.tsx
@@ -1,5 +1,9 @@
 import { atom } from "recoil";
 
+interface Question{
+  question:string
+}
+
 export interface NewJobPosting{
   jobTitle:string,
   jobDescription:string,
@@ -13,6 +17,10 @@ export interface NewJobPosting{
     default: 0 as number, // default value (aka initial value)
   });
 
+export const questionsAtom = atom({
+    key: 'questionsAtom', // unique ID (with respect to other atoms/selectors)
+    default: [] as Array<any> , // default value (aka initial value)
+  });
 
 export const NewJobPostingAtom= atom({
   key:"NewJobPostingAtom",
--- src/pages/newJob/components/CustomQuestionsPage.tsx
+++ src/pages/newJob/components/CustomQuestionsPage.tsx
@@ -1,27 +1,50 @@
 import { useForm } from "react-hook-form";
 import { useRecoilState } from "recoil";
-import NewJobPageIndexAtom, { NewJobPosting, NewJobPostingAtom } from "../atoms/newJobAtoms";
+import NewJobPageIndexAtom, { NewJobPosting, NewJobPostingAtom, questionsAtom } from "../atoms/newJobAtoms";
 import useSubmitOnboardingDataToFirebase from "../../home/logic/useSubmitOnboardingDataToFirebase";
 
 
 export default function CustomQuestionsPage() {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm<NewJobPosting>();
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const [newJobPosting, setNewJobPosting] = useRecoilState(NewJobPostingAtom);
+    const [questions, setQuestions] = useRecoilState(questionsAtom);
 
+
+    const handleAddNewQuestion = (data: any) => {
+        console.log(data.question);
+        setQuestions([...questions, data.question]);
+    }
 
     return (
         <div className="h-full w-full bg-tan justify-center items-center flex flex-col text-left">
 
             <div className="w-[600px] h-[90%] bg-bray rounded-md overflow-y-scroll p-10">
-                <div className="text-white/90 text-6xl font-bold">Post a New Job</div>
-                <div className="text-white/90 text-xl mt-5">Make a new job post to start receiving applications</div>
+                <div className="text-white/90 text-6xl font-bold">Add Custom Questions</div>
+                <div className="text-white/90 text-xl mt-5 mb-10">Filter candidates based on answers</div>
 
-                <div className="text-white ">
-                    {newJobPosting.endSalary.toString()}
-                    {newJobPosting.startSalary.toString()}
-                    {newJobPosting.jobTitle.toString()}
+
+                <form onSubmit={handleSubmit(handleAddNewQuestion)}>
+                    <textarea {...register("question")} placeholder="How many years of experience do you have with React.js?" className=" w-96 border-b-[1px] border-white/90 text-white/90 bg-transparent outline-0 px-2 py-1 mt-3 flex justify-center items-center">
+                    </textarea>
+                    <button type={"submit"} onClick={() => { }} className=" mt-10 hover:text-breen text-white font-bold hover:text-tan flex flex-row gap-5 justify-center items-center px-4 py-2 bg-transparent border-2 border-white/90 hover:bg-white/90 rounded-md">
+                        Add New Question
+                    </button>
+                </form>
+
+                <div className="flex flex-col h-min gap-5 mt-10 w-full flex justify-start items-start">
+                    {
+                        questions.map((e, index) => {
+                            return (
+                                <div className="text-white font-bold text-xl">Q.{index} {e}</div>
+                            )
+                        })
+                    }
                 </div>
+
+                <button onClick={() => { console.log("New Job Posted")}} className=" mt-10 hover:text-breen text-white font-bold hover:text-tan flex flex-row gap-5 justify-center items-center px-4 py-2 bg-transparent border-2 border-white/90 hover:bg-white/90 rounded-md">
+                    Post Job
+                </button>
 
             </div>
 
