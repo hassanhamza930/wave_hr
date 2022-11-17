b6c4724172bf6bfdb8673c8a950c4ad420e4af52
hassanhamza930 <hassanhamza930@gmail.com>
2022-11-17 22:20:30 +0500

    update in all applicants section.

--- src/pages/applicants/components/AllApplicants.tsx
+++ src/pages/applicants/components/AllApplicants.tsx
@@ -84,18 +84,18 @@ export default function AllApplicants() {
             {
                 applicants.map((applicant) => {
                     return (
-                        <button key={`${applicant.id}`} onClick={() => { handleSelectApplicant(applicant) }} className={`hover:bg-bray hover:text-tan hover:scale-[1.02] w-full gap-4 ${applicant.rating==null?"bg-bray/90 text-tan ":"bg-breen/[10%]  text-bray/90"}  rounded-md my-1 flex flex-row justify-start items-center p-3`}>
+                        <button key={`${applicant.id}`} onClick={() => { handleSelectApplicant(applicant) }} className={`hover:bg-bray hover:text-tan hover:scale-[1.02] w-full gap-4 ${applicant.rating==null?"bg-breen/90 text-tan ":"bg-bray/[90%]  text-tan"}  rounded-md my-1 flex flex-row justify-start items-center p-3`}>
 
                             <div style={{ backgroundImage: `url('${applicant.profilePicture}')` }} className='bg-cover bg-center h-12 w-12 bg-breen rounded-md'></div>
 
                             <div className='flex flex-row justify-between items-center w-full'>
 
-                                <div className='flex flex-col w-full justify-center items-start '>
+                                <div className='flex flex-col w-full justify-center items-start pr-2'>
                                     <div className="text-md font-bold ">{applicant.name}</div>
                                     <div className="text-md">{applicant.email}</div>
                                 </div>
 
-                                <div className=' w-full h-full flex justify-end flex-row pr-5 gap-3 items-center'>
+                                <div className=' w-full h-full flex justify-end flex-row pr-3 gap-3 items-center'>
                                     {/* <div className='hidden 2xl:flex px-4 py-2 text-bray text-[12px] rounded-full bg-tan/90'>{applicant.applicationStatus}</div> */}
                                     <div className='px-4 py-2 text-breen text-sm font-bold rounded-md bg-tan/90'>{applicant.rating != null ? applicant.rating : "Pending Review"}</div>
                                 </div>
