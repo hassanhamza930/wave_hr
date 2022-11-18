12fb658516f8f53b8f7f6be84e62c26d817a4bde
Hamza Hassan <hassanhamza930@gmail.com>
2022-11-18 11:16:58 +0500

    All Applicants UI updated.

--- src/pages/applicants/components/AllApplicants.tsx
+++ src/pages/applicants/components/AllApplicants.tsx
@@ -84,7 +84,7 @@ export default function AllApplicants() {
             {
                 applicants.map((applicant) => {
                     return (
-                        <button key={`${applicant.id}`} onClick={() => { handleSelectApplicant(applicant) }} className={`hover:bg-bray hover:text-tan hover:scale-[1.02] w-full gap-4 ${applicant.rating==null?"bg-breen/90 text-tan ":"bg-bray/[90%]  text-tan"}  rounded-md my-1 flex flex-row justify-start items-center p-3`}>
+                        <button key={`${applicant.id}`} onClick={() => { handleSelectApplicant(applicant) }} className={`hover:bg-bray hover:text-tan hover:scale-[1.02] w-full gap-4 ${applicant.rating==null?"bg-bray/90 text-tan ":"bg-bray/[10%] shadow-md border-bray/10  text-bray"}  rounded-md my-1 flex flex-row justify-start items-center p-3`}>
 
                             <div style={{ backgroundImage: `url('${applicant.profilePicture}')` }} className='bg-cover bg-center h-12 w-12 bg-breen rounded-md'></div>
 
