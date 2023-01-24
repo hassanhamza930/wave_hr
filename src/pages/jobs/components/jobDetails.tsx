36e242f11becdd0de682dacf22aa51208eae3a79
Hamza Hassan <hassanhamza930@gmail.com>
2023-01-24 23:53:44 +0500

    minor change in job link copy

--- src/pages/jobs/components/jobDetails.tsx
+++ src/pages/jobs/components/jobDetails.tsx
@@ -14,7 +14,7 @@ export default function JobDetails() {
     const db = getFirestore();
 
     function copyJobLink(id: string) {
-        var jobLink = "http://localhost:3000" + "/apply/" + id;
+        var jobLink = "https://wavehr.vercel.app" + "/apply/" + id;
         navigator.clipboard.writeText(jobLink);
         toast.success("Job Link Copied to Clipboard");
     }
