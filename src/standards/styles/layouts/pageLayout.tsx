341c9e332638a6cf6b262bb5f196de1c5acb65c4
Hamza Hassan <hassanhamza930@gmail.com>
2023-02-09 08:12:58 +0500

    Added FIxed Padding at the bottom.

--- src/standards/styles/layouts/pageLayout.tsx
+++ src/standards/styles/layouts/pageLayout.tsx
@@ -5,6 +5,8 @@ function PageLayout(props: any) {
         <div className="pt-[80px] bg-tan h-screen w-full flex flex-col justify-start items-start">
             <div className="px-[1%] 2xl:px-[5%] pt-5 w-full h-full flex-1 flex-col justify-start items-start overflow-y-scroll ">
                 {children}
+                <div className="mb-24">
+                </div>
             </div>
         </div>
 
