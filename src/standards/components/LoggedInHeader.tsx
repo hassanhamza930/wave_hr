dee50a10e41606f18d8c911459265d038b7515be
Hamza Hassan <hassanhamza930@gmail.com>
2023-04-26 18:49:45 +0500

    Updated Header Implemented.

--- src/standards/components/LoggedInHeader.tsx
+++ src/standards/components/LoggedInHeader.tsx
@@ -17,8 +17,8 @@ export default function LoggedInHeader() {
 
 
     return (
-        <div className="fixed shadow-md shadow-black/10 h-[70px] bg-tan z-50 w-full flex flex-row justify-between items-center px-[5%] py-5">
-         
+        <div className="fixed h-[70px] bg-tan z-50 w-full flex flex-row justify-between items-center px-[5%] py-5">
+
             <div className="text-[24px] font-bold">
                 Jobs
             </div>
@@ -36,50 +36,51 @@ export default function LoggedInHeader() {
 
 
 
-                <Menu >
-                    <Menu.Button onClick={() => { }} className="flex flex-row justify-center items-center gap-2 border-2 p-2 rounded-xl hover:border-blue border-gray">
-                        <div style={{ backgroundImage: `url("${loggedInUser.photoUrl}")` }} className="bg-center bg-blue rounded-xl h-10 w-10 bg-cover" ></div>
-                        <div style={{ backgroundImage: `url("${LogoText}")` }} className="bg-center rounded-xl h-10 w-24 bg-contain bg-no-repeat" ></div>
-                        {/* <AiFillCaretDown></AiFillCaretDown> */}
-                    </Menu.Button>
+                <div className="relative">
+                    <Menu >
+                        <Menu.Button onClick={() => { }} className="flex flex-row justify-center items-center gap-2 border-[2px] p-2 rounded-xl hover:border-blue border-gray">
+                            <div style={{ backgroundImage: `url("${loggedInUser.photoUrl}")` }} className="bg-center bg-blue rounded-xl h-10 w-10 bg-cover" ></div>
+                            <div style={{ backgroundImage: `url("${LogoText}")` }} className="bg-center rounded-xl h-10 w-24 bg-contain bg-no-repeat" ></div>
+                            <AiFillCaretDown size={10}></AiFillCaretDown>
+                        </Menu.Button>
 
-                    <Transition
-                        as={Fragment}
-                        enter="transition ease-out duration-100"
-                        enterFrom="transform opacity-0 scale-95"
-                        enterTo="transform opacity-100 scale-100"
-                        leave="transition ease-in duration-75"
-                        leaveFrom="transform opacity-100 scale-100"
-                        leaveTo="transform opacity-0 scale-95"
-                    >
-                        <Menu.Items className="absolute -mb-36 justify-center right-0 w-36 origin-top-right divide-y divide-gray-100 mr-20  bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                            <div className="">
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => { navigate("/") }}
-                                            className={`${active ? 'bg-secondary text-black' : 'text-gray-900'} group flex w-full items-center justify-start  px-4 py-2 text-sm`}>
-                                            Home
-                                        </button>
-                                    )}
-                                </Menu.Item>
+                        <Transition
+                            as={Fragment}
+                            enter="transition ease-out duration-100"
+                            enterFrom="transform opacity-0 scale-95"
+                            enterTo="transform opacity-100 scale-100"
+                            leave="transition ease-in duration-75"
+                            leaveFrom="transform opacity-100 scale-100"
+                            leaveTo="transform opacity-0 scale-95"
+                        >
+                            <Menu.Items className="absolute mt-2 mr-2 justify-center right-0 w-36 origin-top-right divide-y divide-gray-100 rounded-md bg-tan/90 backdrop-blur-2xl shadow-xl ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                <div className="">
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <button
+                                                onClick={() => { navigate("/") }}
+                                                className={`${active ? 'bg-gray/50 text-black' : ''} group flex w-full items-center justify-start  px-4 py-2 text-sm`}>
+                                                Home
+                                            </button>
+                                        )}
+                                    </Menu.Item>
 
-                                <Menu.Item>
-                                    {({ active }) => (
-                                        <button
-                                            onClick={() => { localStorage.clear(); window.location.href = "/" }}
-                                            className={`${active ? 'bg-secondary text-black' : 'text-gray-900'} group flex w-full items-center justify-start  px-4 py-2 text-sm`}>
-                                            Log Out
-                                        </button>
-                                    )}
-                                </Menu.Item>
+                                    <Menu.Item>
+                                        {({ active }) => (
+                                            <button
+                                                onClick={() => { localStorage.clear(); window.location.href = "/" }}
+                                                className={`${active ? 'bg-gray/50 text-black' : ''} group flex w-full items-center justify-start  px-4 py-2 text-sm`}>
+                                                Log Out
+                                            </button>
+                                        )}
+                                    </Menu.Item>
 
-                            </div>
-                        </Menu.Items>
-                    </Transition>
+                                </div>
+                            </Menu.Items>
+                        </Transition>
 
-
-                </Menu>
+                    </Menu>
+                </div>
 
             </div>
 
