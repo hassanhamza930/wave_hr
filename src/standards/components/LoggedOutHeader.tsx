1facfbaa6a60178d69b2ab09a8759548099446e7
Hunzalaqamar <hunzla.qamar@gmail.com>
2023-04-26 17:20:28 +0500

    landing page started

--- src/standards/components/LoggedOutHeader.tsx
+++ src/standards/components/LoggedOutHeader.tsx
@@ -1,47 +1,50 @@
-import Logo from "../../images/logo.svg";
+import Logo from "../../images/landingPage/wavehr-logo.png";
 import { BsArrowRightShort } from "react-icons/bs";
 import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 
-
 export default function LoggedOutHeader() {
+  const navigate = useNavigate();
+  const [isApplyPage, setIsApplyPage] = useState<boolean>(true);
 
-    const navigate = useNavigate();
-    const [isApplyPage, setIsApplyPage] = useState<boolean>(true);
+  useEffect(() => {
+    if (window.location.pathname.toString().includes("apply") == false) {
+      setIsApplyPage(false);
+    } else {
+      setIsApplyPage(true);
+    }
+  }, []);
 
-    useEffect(() => {
-        if (window.location.pathname.toString().includes("apply") == false) {
-            setIsApplyPage(false);
-        }
-        else {
-            setIsApplyPage(true);
-        }
-    }, [])
+  return (
+    <>
+      {isApplyPage == false && (
+        <div className="fixed h-[80px] bg-white z-50 shadow-md w-full flex flex-row justify-between items-center px-[5%] py-5">
+          <button
+            onClick={() => {
+              navigate("/");
+            }}
+          >
+            <img src={Logo} className="h-full mt-[2px] w-36"></img>
+          </button>
 
-    return (
-        <>
-            {
-                isApplyPage == false &&
-                <div className="fixed h-[80px] bg-transparent z-50 w-full flex flex-row justify-between items-center px-[5%] py-5">
-                    <button onClick={() => { navigate("/") }}>
-                        <img src={Logo} className="h-14 w-14"></img>
-                    </button>
-
-                    <div className="flex flex-row gap-16 justify-between items-center">
-
-                        {/* <button onClick={() => {
+          <div className="flex flex-row gap-16 justify-between items-center">
+            {/* <button onClick={() => {
                             navigate("/pricing");
                         }} className="hover:scale-[1.02] font-bold text-black text-sm">Pricing</button> */}
 
-                        <button onClick={() => { navigate("/login") }} className="hover:scale-[1.02] text-sm px-4 py-2 rounded-md bg-blue flex flex-row gap-2 justify-center items-center">
-                            <div className="text-tan font-medium">Login</div>
-                            <BsArrowRightShort color="white" size={20} />
-                        </button>
-
-                    </div>
-                </div>
-            }
-        </>
-
-    )
+            <button
+              onClick={() => {
+                navigate("/login");
+              }}
+              className="hover:scale-[1.02] text-sm px-8 py-3 rounded-[10px] bg-primary flex flex-row gap-2 justify-center items-center"
+            >
+              <div className="text-tan font-medium text-[10px] uppercase">
+                Sign up now
+              </div>
+            </button>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-   extend:{
-    colors:{
-      "tan":"#FFFFFF",
-      "black":"#2C2B2B",
-      "blue":"#243763",
-      "purp":"#B3005E"
-     }
-   }
+    extend: {
+      colors: {
+        tan: "#FFFFFF",
+        black: "#2C2B2B",
+        blue: "#243763",
+        purp: "#B3005E",
+        primary: "#0161FE",
+      },
+    },
   },
   plugins: [],
-}
+};
