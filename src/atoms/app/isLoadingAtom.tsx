5b48ff3a025392bf574cf964fe060c614058c885
Hamza Hassan <hassanhamza930@gmail.com>
2023-05-01 18:04:49 +0500

    Fixed the loading screen flicker

--- src/atoms/app/isLoadingAtom.tsx
+++ src/atoms/app/isLoadingAtom.tsx
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 
  const isLoadingAtom = atom({
     key: 'isLoadingAtom', // unique ID (with respect to other atoms/selectors)
-    default: false as boolean, // default value (aka initial value)
+    default: true as boolean, // default value (aka initial value)
   });
 
 
