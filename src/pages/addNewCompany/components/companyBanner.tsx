31e9b957b1b0bd44241da5895686a3acafce9d26
Hamza Hassan <hassanhamza930@gmail.com>
2023-05-01 17:42:22 +0500

    Minor changes to company workflow

--- src/pages/addNewCompany/components/companyBanner.tsx
+++ src/pages/addNewCompany/components/companyBanner.tsx
@@ -34,12 +34,12 @@ function CompanyBanner({ customStyles = "", setCompanyBanner, companyBannerValue
     }
 
     return (
-        <div style={{backgroundImage:`url('${companyBannerValue==""?"https://assets-global.website-files.com/5c7fdbdd4e3feeee8dd96dd2/62c4ff55b8637de51557f5f0_growth-flat-color.gif":companyBannerValue}')`}} className={` ${customStyles} w-full h-48 bg-blue bg-cover shadow-xl bg-center rounded-md flex justify-end items-end p-10`}>
+        <div style={{backgroundImage:`url('${companyBannerValue==""?"https://assets-global.website-files.com/5c7fdbdd4e3feeee8dd96dd2/62c4ff55b8637de51557f5f0_growth-flat-color.gif":companyBannerValue}')`}} className={` ${customStyles} w-full h-48 bg-white bg-cover shadow-xl bg-center rounded-md flex justify-end items-end p-10`}>
 
             <div className="flex flex-row justify-end items-center">
                 {/* <SubHeading text="Add company banner (Optional)" color="text-tan/80" customStyles="text-sm pr-5"></SubHeading> */}
-                <button onClick={() => { UpdateCompanyCoverImage() }} className="transition ease-in-out duration-100 hover:scale-105 shadow-md h-14 w-14 flex justify-center items-center rounded-md bg-tan">
-                    <AiFillCamera className="text-blue/70 h-8 w-8" />
+                <button onClick={() => { UpdateCompanyCoverImage() }} className="transition ease-in-out duration-100 hover:scale-105 shadow-md h-14 w-14 flex justify-center items-center rounded-md text-black/80 bg-white">
+                    <AiFillCamera className="h-8 w-8" />
                 </button>
             </div>
 
